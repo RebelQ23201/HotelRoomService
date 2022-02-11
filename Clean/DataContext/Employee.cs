4242540758f440f21615d46683ccbc582cc59bc8
antitank19 <trankhaiminhkhoi10a3@gmail.com>
2022-02-11 14:15:43 +0700

    Fix dbcontext, add mapper

---
+++ Clean/DataContext/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Clean.DataContext
+{
+    public partial class Employee
+    {
+        public Employee()
+        {
+            OrderDetails = new HashSet<OrderDetail>();
+        }
+
+        public int EmployeeId { get; set; }
+        public int? CompanyId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public int? Status { get; set; }
+
+        public virtual Company Company { get; set; }
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
