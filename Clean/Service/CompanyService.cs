98e391a3216c93e7b894be35430c43a0f66a767f
QuangBG <qbuigia@yahoo.com.vn>
2022-02-14 14:24:51 +0700

    add Repo, Service

---
+++ Clean/Service/CompanyService.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Clean.DataContext;
+using Clean.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clean.Service
+{
+    public class CompanyService
+    {
+        private readonly CleanDBContext _context;
+        private readonly IMapper mappper;
+
+        public CompanyService()
+        {
+        }
+
+        public CompanyService(CleanDBContext context, IMapper mappper)
+        {
+            _context = context;
+            this.mappper = mappper;
+        }
+
+        public CompanyModel GetCompany(int id)
+        {
+            var company = _context.Companies.FindAsync(id);
+
+            if (company == null)
+            {
+                return null;
+            }
+
+            CompanyModel model = mappper.Map<CompanyModel>(company);
+
+            return model;
+        }
+    }
+}
