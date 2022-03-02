7e939a5f53fb4e1b8f4630e50976787da9fc3dcc
QuangBG <qbuigia@yahoo.com.vn>
2022-03-02 20:15:29 +0700

    fixThe configured user limit issue

--- Clean/Program.cs
+++ Clean/Program.cs
@@ -18,6 +18,12 @@ namespace Clean
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("appsettings.Development.json",
+                        optional: true,
+                        reloadOnChange: false);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
--- Clean/Startup.cs
+++ Clean/Startup.cs
@@ -81,7 +81,9 @@ namespace Clean
 
             IConfiguration config = new ConfigurationBuilder()
                                    .SetBasePath(Directory.GetCurrentDirectory())
-                                   .AddJsonFile("appsettings.Development.json", true, true)
+                                   .AddJsonFile("appsettings.Development.json", 
+                                            optional: true,
+                                            reloadOnChange: false)
                                    .Build();
             services.AddAuthentication().AddGoogle(googleOptions =>
             {
